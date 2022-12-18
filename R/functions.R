8ae0de9740a4eaa32e34349a336a3a0c363239a9
peterpuetz2020 <ppuetz@uni-goettingen.de>
2022-12-18 15:55:23 +0100

    update

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,15 @@
-get_power <- function(data, cond,  covariate, intercept, beta_cond, beta_cov, beta_int){
+# define function to get power for a range of betas for the interaction 
+# in a logistic regression with one binary intervention variable and one continuous covariate
+# arguments of the function: 
+# "data" is the data frame containing the variables in the model
+# "cond" is the variable name in "data" indicating the (binary) intervention
+# "covariate" is the variable name in "data" indicating the continuous covariate
+# "intercept" is the intercept in the logistic regression model fit by glm
+# "beta_cond" is the coefficient associated with "cond" in the logistic regression model fit by glm
+# "beta_cov" is the coefficient associated with "covariate" in the logistic regression model fit by glm
+# "beta_int" is a range of coefficients associated for the interaction for which the power should be calculated
+
+get_power_logistic <- function(data, cond,  covariate, intercept, beta_cond, beta_cov, beta_int){
   length_list = length(beta_int)
   pmap_dbl(list(df = rep(list(data),length_list),
                          cond = rep(list(cond),length_list), 
@@ -8,25 +19,67 @@ get_power <- function(data, cond,  covariate, intercept, beta_cond, beta_cov, be
                          beta_cov = rep(list(beta_cov),length_list), 
                          beta_int = as.list(beta_range)), 
                     .f = function(df, cond, covariate, intercept, beta_cond, beta_cov, beta_int){
-                      #Run mc replications
+                      # run mc replications
                       tmp_results <- replicate(n = mc, expr = {
+                        # access intervention and covariate
                         cond = df[[cond]]
                         covariate = df[[covariate]]
-                        # Specify the linear predictor for the logistic model
-                        # effects for covariate is set to 0, but
-                        # you can choose any other value
+                        # specify the linear predictor for the logistic model
                         lp = intercept + beta_cond*cond + beta_cov*covariate + beta_int*cond*covariate
-                        # The link function for logistic regression
+                        # apply the link function for logistic regression
                         link_lp = exp(lp)/(1 + exp(lp)) 
                         # generate y on a binary scale including random variation
                         y = (runif(length(lp)) < link_lp) 
-                        # Run a logistic regression
+                        # run a logistic regression
                         log.int = glm(y ~ cond*covariate, family=binomial)
                         # store if p-value of interaction is lower than alpha
                         summary(log.int)$coefficients[4,4] < alpha
                       })
-                      #Compute the power, i.e. share of tests with p < alpha
+                      # compute the power, i.e. share of tests with p < alpha
                       mean(tmp_results)
                     })
+}
+
+
+# define function to get power for a range of betas for the interaction 
+# in a linear regression with one binary intervention variable and one continuous covariate
+# arguments of the function: 
+# "data" is the data frame containing the variables in the model
+# "cond" is the variable name in "data" indicating the (binary) intervention
+# "covariate" is the variable name in "data" indicating the continuous covariate
+# "intercept" is the intercept in the regression model fit by lm
+# "beta_cond" is the coefficient associated with "cond" in the regression model fit by lm
+# "beta_cov" is the coefficient associated with "covariate" in the regression model fit by lm
+# "beta_int" is a range of coefficients associated for the interaction for which the power should be calculated
+# "res_std_error" is the residual standard error in the regression model fit by lm
+
+get_power_lm <- function(data, cond,  covariate, intercept, beta_cond,
+                         beta_cov, beta_int, res_std_error){
+  length_list = length(beta_int)
+  pmap_dbl(list(df = rep(list(data),length_list),
+                cond = rep(list(cond),length_list), 
+                covariate = rep(list(covariate),length_list),
+                intercept = rep(list(intercept),length_list),
+                beta_cond = rep(list(beta_cond),length_list), 
+                beta_cov = rep(list(beta_cov),length_list), 
+                beta_int = as.list(beta_range),
+                res_std_error =  rep(list(res_std_error),length_list)), 
+           .f = function(df, cond, covariate, intercept, beta_cond, beta_cov, beta_int,res_std_error){
+             # run mc replications
+             tmp_results <- replicate(n = mc, expr = {
+               cond = df[[cond]]
+               covariate = df[[covariate]]
+               # specify the linear predictor for the linear model
+               lp = intercept + beta_cond*cond + beta_cov*covariate + beta_int*cond*covariate
+               # add noise
+               y = lp + rnorm(length(lp), 0, res_std_error)
+               # run a regression
+               log.int = lm(y ~ cond*covariate)
+               # store if p-value of interaction is lower than alpha
+               summary(log.int)$coefficients[4,4] < alpha
+             })
+             # compute the power, i.e. share of tests with p < alpha
+             mean(tmp_results)
+           })
 }
 
---
+++ R/nora_lm_sensitivity.R
@@ -0,0 +1,149 @@
+# sensitivity analyses for the interaction effects in study 2
+
+# clear workspace
+rm(list=ls())
+
+# get functions defined in other script
+source("R/functions.R")
+
+# install pacman package if required
+if (!("pacman" %in% installed.packages())) install.packages("pacman") 
+# load required packages
+pacman::p_load(tidyverse, psych, MASS, readxl)
+
+# set random seed to ensure replicability
+set.seed(6)
+
+# set number of simulations
+mc = 5000
+
+# desired power
+power_targeted = 0.8
+
+# set alpha
+alpha = 0.05
+
+# range of interaction effects that should be checked, i.e. for which
+# power will be calculated
+beta_range <- seq(0.10, 0.15, length = 40)
+
+# read in data
+da <- read_excel("data/study2_data.xlsx", 
+                 sheet = "data")
+
+# restrict data to first intervention
+da_intA <-
+  da %>% 
+  filter(!is.na(KGA_IntA)) %>% 
+  # center BioV variable
+  mutate(MW_Bio = scale(MW_Bio, center = TRUE, scale = FALSE))
+
+# restrict data to second intervention
+da_intB <-
+  da %>% 
+  filter(!is.na(KGB_IntB)) %>% 
+  # center BioV variable
+  mutate(MW_Bio = scale(MW_Bio, center = TRUE, scale = FALSE))
+
+## exemplary interpretation
+# fit model
+mod <- lm(Rebound ~ KGA_IntA*MW_Bio, data = da_intA)
+sum_mod <- summary(mod)
+sum_mod
+
+# visualizatin of interaction effect
+# on probability scale
+pred_data <- data.frame(yhat_prob = predict(mod, type = "response"), 
+                        yhat_odds = predict(mod, type = "response")/
+                          (1-predict(mod, type = "response")), 
+                        x = da_intA$MW_Bio,
+                        f = as.factor(da_intA$KGA_IntA))
+ggplot(pred_data, aes(x = x, y = yhat_prob, group = f,
+                      color = f)) + 
+  geom_line() 
+
+# for MW_Bio = 0, the intervention effect decreases the rebound by  0.225 units
+# for the interaction: one additional biosperic value decreases the intervention
+# effect on the rebound
+# by 0.0258877, e.g. when increasing the biosperic value from 0 to 1, 
+# the intervention effect on rebound decreases from
+# -0.2256266 to -0.2515143
+
+
+# Effect of intervention A on Rebound ----------------------------------------------------------------
+# the simulations are time-consuming, if you do not want to run them but 
+# just want to check the results, go to the end of this script
+
+# estimate model
+mod <- lm(Rebound ~ KGA_IntA*MW_Bio, data = da_intA)
+sum_mod <- summary(mod)
+
+# get power for range of betas
+results <- get_power_lm(data = da_intA,
+                              cond ="KGA_IntA",
+                              cov = "MW_Bio",
+                              intercept = mod$coefficients["(Intercept)"],
+                              beta_cond = mod$coefficients["KGA_IntA"],
+                              beta_cov = mod$coefficients["MW_Bio"],
+                              beta_int = beta_range, 
+                              res_std_error = sum_mod$sigma)
+
+# save results
+write.table(results, "results/lm_power_inta_rebound.dat", row.names = FALSE, col.names = FALSE)
+
+# Effect of intervention B on Rebound ----------------------------------------------------------------
+# the simulations are time-consuming, if you do not want to run them but 
+# just want to check the results, go to the end of this script
+
+# estimate model
+mod <- lm(Rebound ~ KGB_IntB*MW_Bio, data = da_intB)
+sum_mod <- summary(mod)
+
+# get power for range of betas
+results <- get_power_lm(data = da_intB,
+                        cond ="KGB_IntB",
+                        cov = "MW_Bio",
+                        intercept = mod$coefficients["(Intercept)"],
+                        beta_cond = mod$coefficients["KGB_IntB"],
+                        beta_cov = mod$coefficients["MW_Bio"],
+                        beta_int = beta_range, 
+                        res_std_error = sum_mod$sigma)
+
+# save results
+write.table(results, "results/lm_power_intb_rebound.dat", row.names = FALSE, col.names = FALSE)
+
+# summarise results -------------------------------------------------------
+# read in results data
+results_inta_rebound <- read.table("results/lm_power_inta_rebound.dat")$V1
+results_intb_rebound <- read.table("results/lm_power_intb_rebound.dat")$V1
+
+# plot power for range of interaction effects in model with 
+# intervention a and dependent variable rebound
+plotdat_inta_rebound <- data.frame(coefficient = beta_range,
+                               power = results_inta_rebound)
+ggplot(plotdat_inta_rebound, aes(x = coefficient, y = power)) +
+  geom_path()+
+  geom_hline(yintercept = power_targeted, linetype = 2)+
+  labs(x = "Effect size (coefficient)", y = "Power")+
+  theme_bw()
+
+# plot power for range of interaction effects in model with 
+# intervention b and dependent variable rebound
+plotdat_intb_rebound <- data.frame(coefficient = beta_range,
+                                   power = results_intb_rebound)
+ggplot(plotdat_intb_rebound, aes(x = coefficient, y = power)) +
+  geom_path()+
+  geom_hline(yintercept = power_targeted, linetype = 2)+
+  labs(x = "Effect size (coefficient)", y = "Power")+
+  theme_bw()
+
+# show odds ratio for interaction effect of intervention a and biosperic value
+# on rebound that corresponds (nearly) to a power of 0.8 
+plotdat_inta_rebound %>% 
+  mutate(deviation = abs(power-power_targeted)) %>% 
+  filter(deviation == min (deviation))
+# show odds ratio for interaction effect of intervention b and biosperic value
+# on rebound that corresponds (nearly) to a power of 0.8 
+plotdat_intb_rebound %>% 
+  mutate(deviation = abs(power-power_targeted)) %>% 
+  filter(deviation == min (deviation))
